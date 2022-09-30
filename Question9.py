3c64e99eabbeac9335e037c7edd63ebed5b695ad
BairagiTarun <112105779+BairagiTarun@users.noreply.github.com>
2022-10-01 00:47:01 +0530

    Create Question9.py

---
+++ Question9.py
@@ -0,0 +1,5 @@
+Name=input('Enter Your Name:')
+Age=int(input('Enter Your Age:'))
+Qualif=input('Enter Your Qualification:')
+Year_Ex=int(input("Enter Your Years Of Experience:"))
+print(Name,Age,Qualif,Year_Ex,sep='\n')
